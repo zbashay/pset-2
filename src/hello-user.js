cb8002f9575f5d0b0c775ba73b24f01be49676f1
Zaviay Bashay <ZBashay@ucvts.tec.nj.us>
2019-10-18 10:37:41 -0400

    hbd Vanya

--- src/hello-user.js
+++ src/hello-user.js
@@ -1,4 +1,4 @@
 const readlineSync = require("readline-sync");
 
 const name = readlineSync.question("Hi! What's your name?\n");
-console.log("Hello, " + name + "!");
+console.log("\nHello, " + name + "!");
